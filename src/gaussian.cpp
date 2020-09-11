9d60b3c035f91e2d9d236ec7b694c006b935d377
J-Lili <janzerli@gmail.com>
2020-09-11 13:55:12 +0200

    Adding hypothesis testing

--- src/gaussian.cpp
+++ src/gaussian.cpp
@@ -1,5 +1,6 @@
 
 #include "utilities.h"
+#include <vector>
 
 void Free_memo(double *a, double *r, int *e1) {
   Free(a);
@@ -7,31 +8,78 @@ void Free_memo(double *a, double *r, int *e1) {
   Free(e1);
 }
 
+
+#ifndef Pi 
+#define Pi 3.141592653589793238463
+#endif 
+
+
+double pnorm(double x, double mean, double std_dev) {
+  double const a1 = 0.31938153, a2 = -0.356563782, a3 = 1.781477937;
+  double const a4 = -1.821255978, a5 = 1.330274429;
+  
+  double x_abs = fabs(x);
+  double K = 1.0 / (1.0 + 0.2316419 * x_abs);
+  double w = 1.0 - 1.0 / sqrt(2 * Pi) * exp(- x_abs * x_abs / 2) * (a1 * K + a2 * K * K + a3 * pow(K,3) + a4 * pow(K,4) + a5 * pow(K,5));
+  
+  if (x < 0 ){
+    w= 1.0 - w;
+  }
+  return w;
+}
+
+
+// true if hypothesis is accepted, false if not
+// hypothesis: sampling a distribution with mean more extreme than lambda 
+// (<-[lambda], >[lambda]), variance [sample_var] we got [mean]
+bool is_hypothesis_accepted (double lambda,double sample_var, double mean, double alpha) {
+  if (lambda<=abs(mean)) return true;
+  double p_value = pnorm(abs(mean),lambda,sqrt(sample_var))-pnorm(-abs(mean),lambda,sqrt(sample_var));
+  return (p_value>alpha);
+}
+
+
+
 // check KKT conditions over features in the rest set
 int check_inactive_set(int *e1, vector<double> &z, XPtr<BigMatrix> xpMat, int *row_idx, 
-                   vector<int> &col_idx, NumericVector &center, NumericVector &scale, double *a,
-                   double lambda, double sumResid, double alpha, double *r, double *m, int n, int p) {
+                       vector<int> &col_idx, NumericVector &center, NumericVector &scale, double *a,
+                       double lambda, double sumResid, double alpha, double *r, double *m, int n, int p, vector<int> steps) {
   
   MatrixAccessor<double> xAcc(*xpMat);
-  double *xCol, sum, l1, l2;
+  double *xCol, sum, sqr_sum, l1, l2;
+  int nsample = n;
   int j, jj, violations = 0;
-  #pragma omp parallel for private(j, sum, l1, l2) reduction(+:violations) schedule(static) 
+#pragma omp parallel for private(j, sum, sqr_sum, l1, l2) reduction(+:violations) schedule(static) 
   for (j = 0; j < p; j++) {
     if (e1[j] == 0) {
       jj = col_idx[j];
       xCol = xAcc[jj];
       sum = 0.0;
-      for (int i=0; i < n; i++) {
+      sqr_sum = 0.0;
+      for (int i=0; i < nsample; i++) {
         sum = sum + xCol[row_idx[i]] * r[i];
+        sqr_sum = sqr_sum + (xCol[row_idx[i]] * r[i]) * (xCol[row_idx[i]] * r[i]);
       }
-      z[j] = (sum - center[jj] * sumResid) / (scale[jj] * n);
+      z[j] = (sum * n / nsample - center[jj] * sumResid) / (scale[jj] * n);
       
       l1 = lambda * m[jj] * alpha;
       l2 = lambda * m[jj] * (1 - alpha);
-      if (fabs(z[j] - a[j] * l2) > l1) {
-        e1[j] = 1;
-        violations++;
+      
+      double variance=sqr_sum/nsample-sum/nsample*sum/nsample;
+      
+      if (is_hypothesis_accepted(lambda, sqrt(variance) / nsample, (z[j]-a[j] * l2), 0.01)) {
+        steps.push_back(n);
+        sum = 0.0;
+        for (int i=0; i < n; i++) {
+          sum = sum + xCol[row_idx[i]] * r[i];
+        }
+        if (fabs(z[j] - a[j] * l2) > l1) {
+          e1[j] = 1;
+          violations++;
+        }
       }
+      else steps.push_back(nsample);
+      
     }
   }
   return violations;
@@ -39,11 +87,11 @@ int check_inactive_set(int *e1, vector<double> &z, XPtr<BigMatrix> xpMat, int *r
 
 // Coordinate descent for gaussian models
 RcppExport SEXP cdfit_gaussian(SEXP X_, SEXP y_, SEXP row_idx_, 
-                                   SEXP lambda_, SEXP nlambda_, 
-                                   SEXP lam_scale_, SEXP lambda_min_, 
-                                   SEXP alpha_, SEXP user_, SEXP eps_, 
-                                   SEXP max_iter_, SEXP multiplier_, SEXP dfmax_, 
-                                   SEXP ncore_, SEXP verbose_) {
+                               SEXP lambda_, SEXP nlambda_, 
+                               SEXP lam_scale_, SEXP lambda_min_, 
+                               SEXP alpha_, SEXP user_, SEXP eps_, 
+                               SEXP max_iter_, SEXP multiplier_, SEXP dfmax_, 
+                               SEXP ncore_, SEXP verbose_) {
   Rprintf("Success in modifying code\n");
   XPtr<BigMatrix> xMat(X_);
   double *y = REAL(y_);
@@ -61,7 +109,7 @@ RcppExport SEXP cdfit_gaussian(SEXP X_, SEXP y_, SEXP row_idx_,
   int max_iter = INTEGER(max_iter_)[0];
   double *m = REAL(multiplier_);
   int dfmax = INTEGER(dfmax_)[0];
-
+  
   NumericVector lambda(L);
   NumericVector center(p);
   NumericVector scale(p);
@@ -73,7 +121,7 @@ RcppExport SEXP cdfit_gaussian(SEXP X_, SEXP y_, SEXP row_idx_,
   double *lambda_max_ptr = &lambda_max;
   int xmax_idx = 0;
   int *xmax_ptr = &xmax_idx;
-
+  
   // set up omp
   int useCores = INTEGER(ncore_)[0];
 #ifdef BIGLASSO_OMP_H_
@@ -84,7 +132,7 @@ RcppExport SEXP cdfit_gaussian(SEXP X_, SEXP y_, SEXP row_idx_,
   omp_set_dynamic(0);
   omp_set_num_threads(useCores);
 #endif
- 
+  
   if (verbose) {
     char buff1[100];
     time_t now1 = time (0);
@@ -95,9 +143,9 @@ RcppExport SEXP cdfit_gaussian(SEXP X_, SEXP y_, SEXP row_idx_,
   // standardize: get center, scale; get p_keep_ptr, col_idx; get z, lambda_max, xmax_idx;
   standardize_and_get_residual(center, scale, p_keep_ptr, col_idx, z, lambda_max_ptr, xmax_ptr, xMat, 
                                y, row_idx, lambda_min, alpha, n, p);
-
+  
   p = p_keep;   // set p = p_keep, only loop over columns whose scale > 1e-6
-
+  
   if (verbose) {
     char buff1[100];
     time_t now1 = time (0);
@@ -105,7 +153,7 @@ RcppExport SEXP cdfit_gaussian(SEXP X_, SEXP y_, SEXP row_idx_,
     Rprintf("Preprocessing end: %s\n", buff1);
     Rprintf("\n-----------------------------------------------\n");
   }
-
+  
   // Objects to be returned to R
   arma::sp_mat beta = arma::sp_mat(p, L); // beta
   double *a = Calloc(p, double); //Beta from previous iteration
@@ -122,6 +170,7 @@ RcppExport SEXP cdfit_gaussian(SEXP X_, SEXP y_, SEXP row_idx_,
   double sumResid = sum(r, n);
   loss[0] = gLoss(r,n);
   thresh = eps * loss[0] / n;
+  vector<int> steps; //Beta from previous iteration
   
   // set up lambda
   if (user == 0) {
@@ -144,7 +193,7 @@ RcppExport SEXP cdfit_gaussian(SEXP X_, SEXP y_, SEXP row_idx_,
     lstart = 0;
     lambda = Rcpp::as<NumericVector>(lambda_);
   }
-
+  
   // Path
   for (l = lstart; l < L; l++) {
     if(verbose) {
@@ -200,14 +249,19 @@ RcppExport SEXP cdfit_gaussian(SEXP X_, SEXP y_, SEXP row_idx_,
       }
       
       // Scan for violations in inactive set
-      violations = check_inactive_set(e1, z, xMat, row_idx, col_idx, center, scale, a, lambda[l], sumResid, alpha, r, m, n, p); 
+      violations = check_inactive_set(e1, z, xMat, row_idx, col_idx, center, scale, a, lambda[l], sumResid, alpha, r, m, n, p, steps); 
       if (violations==0) {
         loss[l] = gLoss(r, n);
         break;
       }
     }
+    int steps_sum = 0;
+    for (int i = 0 ; i < steps.size(); i++) {
+      steps_sum += steps[i];
+    }
+    Rprintf("\n Avg steps: %d\n", ((double)steps_sum)/steps.size());
   }
-
+  
   Free_memo(a, r, e1);
   return List::create(beta, center, scale, lambda, loss, iter, n_reject, Rcpp::wrap(col_idx));
 }
