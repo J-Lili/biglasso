c2924cb35e3bbdf8c1bbe684be6bf1ca27ab0093
J-Lili <janzerli@gmail.com>
2020-09-18 13:40:08 +0200

    Only checking n/10 examples by default

--- src/gaussian_hsr_edpp_turbo.cpp
+++ src/gaussian_hsr_edpp_turbo.cpp
@@ -18,7 +18,7 @@ int check_strong_set(int *e1, int *e2, vector<double> &z, XPtr<BigMatrix> xpMat,
   double *xCol, sum, sqrsum, l1, l2;
   int j, jj, violations = 0;
   
-  int nsample = n;
+  int nsample = n / 10;
   
   
 #pragma omp parallel for private(j, sum, l1, l2) reduction(+:violations, steps, stepsum) schedule(static) 
@@ -43,16 +43,13 @@ int check_strong_set(int *e1, int *e2, vector<double> &z, XPtr<BigMatrix> xpMat,
       var[j] += variance / nsample;
       sum_prev[j] = sum_prev[j] - sum * n / nsample;
       z[j] = (sum_prev[j] - center[jj] * sumResid) / (scale[jj] * n);
-      Rprintf("%d %d\n",j, newly_entered[j]);
       if (newly_entered[j] || is_hypothesis_accepted(l1,  (z[j]-a[j] * l2), sqrt(var[j])/scale[jj] ,0.001)) {
-       
         steps++;
         stepsum += n;
         sum = 0.0;
         for (int i=0; i < n; i++) {
           sum = sum + xCol[row_idx[i]] * r[i];
         }
-        if (!newly_entered[j] && fabs(sum-sum_prev[j])>0.006) Rprintf("different %d %f %f\n", j, sum, sum_prev[j]);
         newly_entered[j] = false;
         sum_prev[j] = sum;        
         var[j] = 0;
