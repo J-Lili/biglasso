9c1bb5a6b7905c42bdefb67576a92a4b3be388fd
J-Lili <janzerli@gmail.com>
2020-09-21 20:08:01 +0200

    Fixing calculation of sequential rule

--- src/gaussian_turbo.cpp
+++ src/gaussian_turbo.cpp
@@ -21,29 +21,31 @@ int check_inactive_set(int *e1, vector<double> &z, XPtr<BigMatrix> xpMat, int *r
       xCol = xAcc[jj];
       sum = 0.0;
       sqr_sum = 0.0;
+      int current_sample = 50;
       nsample = 50;      
       
       double current_scale = (scale[jj] * n);
-      
+      double variance;
       l1 = lambda * m[jj] * alpha;
       l2 = lambda * m[jj] * (1 - alpha);
       
       do {        
-        nsample = nsample * 2;
-         for (int i = start_pos[j]; i < start_pos[j] + nsample; i++) {
+        current_sample = nsample;
+         for (int i = start_pos[j]; i < start_pos[j] + current_sample; i++) {
           double current_sample = xCol[row_idx[i]] * r_diff[i];
           sum = sum + current_sample;
           sqr_sum = sqr_sum + current_sample * current_sample;
         }      
-        double variance= (sqr_sum/nsample-sum/nsample*sum/nsample);
+        variance = (sqr_sum/nsample-sum/nsample*sum/nsample);
+        start_pos[j] = (start_pos[j]+3*current_sample < n) ?  start_pos[j]+current_sample : 0;        
+        nsample = nsample + current_sample; 
 
-        start_pos[j] = (start_pos[j]+2*nsample < n) ?  start_pos[j]+nsample : 0;
-        var[j] += variance / nsample;
-        sum_prev[j] = sum_prev[j] - sum * n / nsample;
-        z[j] = (sum_prev[j] - center[jj] * sumResid) / current_scale;
-      }
-      while (nsample<n/4 && is_hypothesis_accepted(l1,  (z[j]-a[j] * l2), sqrt(var[j])/scale[jj] ,0.0001));
-     
+        z[j] = ((sum_prev[j] - sum * n / nsample) - center[jj] * sumResid) / current_scale;
+      }  while (nsample<n/4 && is_hypothesis_accepted(l1,  (z[j]-a[j] * l2), sqrt(var[j] + variance / nsample)/scale[jj] ,0.0001));
+      
+      sum_prev[j] -= sum * n / nsample;
+      var[j] += variance / nsample;
+
   
       if (is_hypothesis_accepted(l1,  (z[j]-a[j] * l2), sqrt(var[j])/scale[jj] ,0.0001)) {
         stepsum += n;
