ea1d708e3e0d4ebfe0328a6c90e00e6c1084ebeb
J-Lili <janzerli@gmail.com>
2020-09-24 21:30:50 +0100

    Debugging

--- src/gaussian_turbo.cpp
+++ src/gaussian_turbo.cpp
@@ -59,7 +59,7 @@ int check_inactive_set(int *e1, vector<double> &z, XPtr<BigMatrix> xpMat, int *r
         double true_z = (true_sum - center[jj] * sumResid) / (scale[jj] * n);
         Rprintf("%d: %f %f %f var %.4e current %.4e scaled %.4e__ %d\n",j,true_z - a[j] * l2, z[j] - a[j] * l2, l1, var[j], variance, variance/nsample, start_pos[j]);
         Rprintf("diff est true %f %f %f __ cumulative est true %f %f\n", sum*n/nsample, true_diff, sum, sum_prev[j], true_sum);
-        Rprintf("scale %f variance %f", scale[jj], true_variance);
+        Rprintf("variance %.4e\n", true_variance);
         if (is_hypothesis_accepted(l1,  (z[j]-a[j] * l2), sqrt(var[j]) ,0.0001)) {
           Rprintf("accepted\n");
         }
